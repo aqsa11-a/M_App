1c1dbec9202609c6ad91ad3cdf420afec28d7c09
aqsa11-a <iqbalaqsa793@gmail.com>
2025-03-06 17:48:06 -0800

    Add files via upload
    Second_assignment

--- main.dart
+++ main.dart
@@ -1,69 +1,89 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Basic counter app',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 5, 2, 150)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'My first project in flutter'),
+      home: MyPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() 
-  {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.pink,
-        title: Text(widget.title,style: TextStyle(color:Colors.white),),
+        backgroundColor: const Color.fromARGB(255, 12, 34, 44),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/logo.jfif', height: 40),
+            SizedBox(width: 10),
+            Text(
+              "....... Baba Guru Nanak University",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontFamily: 'Poppins',
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                "Baba Guru Nanak University (BGNU), Nankana Sahib, is a prestigious institution committed to quality education, research, and innovation. Named after Guru Nanak Dev Ji, it promotes academic excellence, cultural diversity, and interfaith harmony, shaping future leaders in Pakistan.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Poppins',
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/uni.jfif',
+                height: 250,
+                width: 300,
+                fit: BoxFit.cover,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.book),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blueGrey[900],
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Text(
+            "AQSA IQBAL",  
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontFamily: 'Poppins',
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
